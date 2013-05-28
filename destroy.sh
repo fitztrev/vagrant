0b0cccf153d77bdae4c90c10c10a33e81bdf9bb3
Trevor Fitzgerald <tfitzgerald@intlbancard.com>
2013-05-28 17:03:40 -0400

    "Enter" help text to delete

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-read -p "Are you sure you want to destroy all of your Vagrant VMs?"
+read -p "Are you sure you want to destroy all of your Vagrant VMs? [Enter] "
 
 vagrant destroy -f
 rm -rf ~/.vagrant.d/boxes/*
